59014177a77ad0d9ecc0c1187585c7e3ed9e557d
Sang Putu Sandhyana Yogi <79888425+youronlydimwit@users.noreply.github.com>
2024-03-21 15:50:46 +0700

    Update dataGenerator.js

--- scripts/dataGenerator.js
+++ scripts/dataGenerator.js
@@ -1,3 +1,10 @@
+function showInputFields() {
+    var dataType = document.getElementById("dataType").value;
+    document.getElementById("rangeInput").style.display = dataType === "numbersInRange" ? "block" : "none";
+    document.getElementById("singleInput").style.display = dataType === "singleNumber" || dataType === "singleWord" ? "block" : "none";
+    document.getElementById("wordsInput").style.display = dataType === "words" ? "block" : "none";
+}
+
 function generateData() {
     var columnName = document.getElementById("columnName").value;
     var numRows = parseInt(document.getElementById("numRows").value);
@@ -12,25 +19,29 @@ function generateData() {
             }
             break;
         case "numbersInRange":
-            var min = parseInt(prompt("Enter the minimum value:"));
-            var max = parseInt(prompt("Enter the maximum value:"));
+            var min = parseInt(document.getElementById("minValue").value);
+            var max = parseInt(document.getElementById("maxValue").value);
             for (var i = 0; i < numRows; i++) {
                 generatedData += "<tr><td>" + (Math.floor(Math.random() * (max - min + 1)) + min) + "</td></tr>";
             }
             break;
         case "singleNumber":
-            var number = parseInt(prompt("Enter the number:"));
-            generatedData += "<tr><td>" + number + "</td></tr>";
+            var number = document.getElementById("singleValue").value;
+            for (var i = 0; i < numRows; i++) {
+                generatedData += "<tr><td>" + number + "</td></tr>";
+            }
             break;
         case "singleWord":
-            var word = prompt("Enter the word:");
-            generatedData += "<tr><td>" + word + "</td></tr>";
+            var word = document.getElementById("singleValue").value;
+            for (var i = 0; i < numRows; i++) {
+                generatedData += "<tr><td>" + word + "</td></tr>";
+            }
             break;
         case "words":
-            var wordsInput = prompt("Enter words separated by commas:");
+            var wordsInput = document.getElementById("wordsValue").value;
             var words = wordsInput.split(",");
-            for (var i = 0; i < words.length; i++) {
-                generatedData += "<tr><td>" + words[i].trim() + "</td></tr>";
+            for (var i = 0; i < numRows; i++) {
+                generatedData += "<tr><td>" + words[i % words.length].trim() + "</td></tr>";
             }
             break;
         default:
